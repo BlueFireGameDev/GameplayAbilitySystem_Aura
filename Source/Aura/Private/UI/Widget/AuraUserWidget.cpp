3281bd37e288241f7870c1d01a6dd449e3ff7f83
Thomas <BlueFireGameDev@gmail.com>
2024-03-07 11:33:42 -0500

    Aura User widget and widget Controller

---
+++ Source/Aura/Private/UI/Widget/AuraUserWidget.cpp
@@ -0,0 +1,10 @@
+// Copyright BlueFireGames Dev
+
+
+#include "UI/Widget/AuraUserWidget.h"
+
+void UAuraUserWidget::SetWidgetController(UObject* InWidgetController)
+{
+	WidgetController = InWidgetController;
+	WidgetControllerSet();
+}
