a7ec8ca6a8333a1590ac650f2202f082c7e7e75e
Thomas <BlueFireGameDev@gmail.com>
2024-04-26 15:26:45 -0400

    Custom Ability Tasks

--- Source/Aura/Private/AbilitySystem/Abilities/AuraProjectileSpell.cpp
+++ Source/Aura/Private/AbilitySystem/Abilities/AuraProjectileSpell.cpp
@@ -12,12 +12,15 @@ void UAuraProjectileSpell::ActivateAbility(const FGameplayAbilitySpecHandle Hand
                                            const FGameplayEventData* TriggerEventData)
 {
 	Super::ActivateAbility(Handle, ActorInfo, ActivationInfo, TriggerEventData);
-	const bool bIsServer =  HasAuthority(&ActivationInfo);
+}
 
+void UAuraProjectileSpell::SpawnProjectile()
+
+{
+	const bool bIsServer =  GetAvatarActorFromActorInfo()->HasAuthority();
+	
 	if (!bIsServer) return;
-
 	ICombatInterface* CombatInterface = Cast <ICombatInterface>(GetAvatarActorFromActorInfo());
-
 	if (CombatInterface)
 	{
 		const FVector SocketLocation =  CombatInterface->GetCombatSocketLocation();
@@ -35,10 +38,6 @@ void UAuraProjectileSpell::ActivateAbility(const FGameplayAbilitySpecHandle Hand
 		// TODO: Give the Projectile a Gameplay Effect Spec for Causing Damage.
 
 		Projectile->FinishSpawning(SpawnTransform);
-
-		
 	}
-
-	
 
 }
